6658cf9ca5bdb31ff3f4e45bd2a6dcba8c119095
femkeboermans <femkeboermans@gmail.com>
2017-01-31 11:28:47 +0100

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # for the Creative bootstrap.. info https://github.com/MrPowers/frontend-generators
+get 'creatives/index'
+
 end
