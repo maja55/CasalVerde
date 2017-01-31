f4913cd1e143fdc1b19b11c84ef9ec47bd0146e9
mippet <pimluitjes@gmail.com>
2017-01-31 10:41:25 +0100

    added methods to controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,20 @@
 class PhotosController < ApplicationController
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:image_url)
+  end
 end
