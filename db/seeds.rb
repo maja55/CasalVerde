e6efbb8a026804b59072f1484fae55a52bbb62fc
mippet <pimluitjes@gmail.com>
2017-02-01 18:00:20 +0100

    fixed little mistakes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@ Content.delete_all
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Photo.delete_all
+
 photo1 = Photo.create(remote_image_url:"http://res.cloudinary.com/mippet/image/upload/v1477406235/Casalverde/index_photo_1.jpg")
 photo2 = Photo.create(remote_image_url:"http://res.cloudinary.com/mippet/image/upload/v1477406220/Casalverde/index_photo_2.jpg")
 photo3 = Photo.create(remote_image_url:"http://res.cloudinary.com/mippet/image/upload/v1477406237/Casalverde/index_photo_3.jpg")
@@ -15,4 +17,3 @@ photo5 = Photo.create(remote_image_url:"http://res.cloudinary.com/mippet/image/u
 photo6 = Photo.create(remote_image_url:"http://res.cloudinary.com/mippet/image/upload/v1477406223/Casalverde/index_photo_6.jpg")
 
 villa_description = Content.create(paragraph: "Villa description bla bla")
-
