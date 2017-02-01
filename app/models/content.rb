824a94e3aa9a4d7640f1fc83618cea3b00b79571
maja55 <majapodrug2@gmail.com>
2017-02-01 08:57:50 +0100

    re-model content, add validation test

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,11 +1,4 @@
 class Content < ApplicationRecord
-  validates :title, length: {maximum: 200}, :allow_blank => true
-  # , if: :has_any_content?
   validates :paragraph, :presence => true
-  # validates :list, :allow_blank => true, if: :has_any_content?
-  #
-  # def has_any_content?
-  #   title.present? || paragraph.present? || list.present?
-  # end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131133802) do
+ActiveRecord::Schema.define(version: 20170201075128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contents", force: :cascade do |t|
-    t.string   "title"
     t.text     "paragraph"
-    t.text     "list"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
