deda49f8c03005bead49023ad587f1ae73c4b308
a-bolz <andreasbolz@gmail.com>
2017-02-01 13:24:22 +0100

    setting up question templates and automated sending/destroying

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,8 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
      if @question.save
+       QuestionMailer.new_question(@question).deliver_now
+       @question.destroy
        redirect_to root_path, notice: "Question succesfully submitted"
      end
   end
