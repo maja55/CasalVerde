039ba6b99b7c2d1994b7870b49c597d4abfbf15b
maja55 <majapodrug2@gmail.com>
2017-02-02 17:07:09 +0100

    carousel2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,24 +17,24 @@
 //= require bootstrap
 //= require tinymce
 //= require tinymce-jquery
+// Instantiate the Bootstrap carousel
 
-$('#myCarousel').carousel({
-  interval: 1000
+$('.multi-item-carousel').carousel({
+  interval: false
 });
 
-$('.carousel .item').each(function(){
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
   var next = $(this).next();
   if (!next.length) {
     next = $(this).siblings(':first');
   }
   next.children(':first-child').clone().appendTo($(this));
 
-  for (var i=0;i<2;i++) {
-    next=next.next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-
-    next.children(':first-child').clone().appendTo($(this));
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
   }
 });
