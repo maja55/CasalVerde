d896848d92e881feae0a1cd9776b5385eea66550
mippet <pimluitjes@gmail.com>
2017-02-02 18:00:53 +0100

    routes test content

--- spec/controllers/contents_controller_spec.rb
+++ spec/controllers/contents_controller_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ContentsController, type: :controller do
-  it { should route(:get, '/contents/new').to(action: :new) }
-  it { should route(:post, '/contents').to(action: :create) }
-  it { should route(:delete, '/contents/1').to(action: :destroy, id: 1) }
+  it { should route(:get, '/contents').to(action: :index) }
+  it { should route(:get, '/contents/1/edit').to(action: :edit, id:1) }
   it { should route(:patch, '/contents/1').to(action: :update, id:1) }
 
 end
--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Content, type: :model do
-  it { should validate_presence_of(:paragraph) }
+  it { should validate_presence_of(:paragraph)
+
+  
 end
