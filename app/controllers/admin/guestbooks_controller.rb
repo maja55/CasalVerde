571368fa9d3ec58fba5a313f03010b15f2436db4
timkuilman <tim.kuilman@gmail.com>
2017-02-03 13:35:56 +0100

    Publish or unpublish guestbook without ajax :(

--- app/controllers/admin/guestbooks_controller.rb
+++ app/controllers/admin/guestbooks_controller.rb
@@ -1,6 +1,30 @@
 class Admin::GuestbooksController < Admin::BaseController
   def index
-    @guestbooks = Guestbook.all
+    @guestbooks_published = Guestbook.all.where(published: true)
+    @guestbook_unpublished = Guestbook.all.where(published: false)
   end
 
+  def update
+    guestbook = Guestbook.find(params[:id])
+    if guestbook.published == true
+      guestbook.published = false
+    else
+      guestbook.published = true
+    end
+    guestbook.save
+
+    respond_to do |f|
+      f.html { redirect_to admin_guestbooks_path }
+    end
+
+  end
+
+  def destroy
+    @guestbook = Guestbook.destroy(params[:id])
+
+    respond_to do |f|
+      f.html { redirect_to admin_guestbooks_path }
+    end
+
+  end
 end
