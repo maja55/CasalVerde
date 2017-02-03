fa153a525f8cf2ffdc547736ab71fb047a199486
mippet <pimluitjes@gmail.com>
2017-02-03 15:15:51 +0100

    guestbook model validation tests

--- spec/controllers/guestbooks_controller_spec.rb
+++ spec/controllers/guestbooks_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe GuestbooksController, type: :controller do
+  it { should route(:get, '/guestbooks').to(action: :index) }
+  it { should route(:get, '/guestbooks/new').to(action: :new) }
+  it { should route(:post, '/guestbooks').to(action: :create) }
+  it { should route(:get, '/guestbooks/1').to(action: :show, id: 1) }
 
 end
