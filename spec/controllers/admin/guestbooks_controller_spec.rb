af9a8adcc20b725bfd9c34ca5031301e14800ab7
mippet <pimluitjes@gmail.com>
2017-02-03 15:20:56 +0100

    admin guestbook controller test

--- spec/controllers/admin/guestbooks_controller_spec.rb
+++ spec/controllers/admin/guestbooks_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Admin::GuestbooksController, type: :controller do
-
+  it { should route(:get, '/admin/guestbooks').to(action: :index) }
+  it { should route(:get, '/admin/guestbooks/new').to(action: :new) }
+  it { should route(:patch, '/admin/guestbooks/1').to(action: :update, id: 1) }
+  it { should route(:get, '/admin/guestbooks/1/edit').to(action: :edit, id: 1) }
+  it { should route(:delete, '/admin/guestbooks/1').to(action: :destroy, id:1) }
 end
