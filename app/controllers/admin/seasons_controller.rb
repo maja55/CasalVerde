cee5cf53d9914de965e946b36cff838e1083e09b
timkuilman <tim.kuilman@gmail.com>
2017-02-03 16:55:54 +0100

    Logout admin link fixed

--- app/controllers/admin/seasons_controller.rb
+++ app/controllers/admin/seasons_controller.rb
@@ -2,4 +2,16 @@ class Admin::SeasonsController < Admin::BaseController
   def index
     @seasons = Season.all
   end
+
+  def new
+    @season = Season.new
+  end
+
+  def create
+
+  end
+  
+  def destroy
+
+  end
 end
